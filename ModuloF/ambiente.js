1addcebceaff1f1e91f50de6de5a58729dd008c2
Issaque Batista Barbosa <isaquebatista400@gmail.com>
2024-03-30 15:40:56 -0300

    Fim modulo F | Desafios pendentes

---
+++ ModuloF/ambiente.js
@@ -0,0 +1,15 @@
+let num = [5, 8, 3, 9, 4, 2, 4, 5]
+/*
+//maneira paia
+console.log(num.length)
+for (let pos = 0; pos < num.length; pos++) {
+  console.log(num[pos])
+}
+
+//maneira mais fácil
+for (let pos in num) {
+  console.log(num[pos])
+}*/
+console.log(num)
+num[3] = 2
+console.log(num)
