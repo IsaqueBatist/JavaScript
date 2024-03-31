f792d41b582886bce0c494faab1707c273643322
Issaque Batista Barbosa <isaquebatista400@gmail.com>
2024-03-31 09:24:27 -0300

    Ajustes

--- Desafios/Desafio1/javaScript.js
+++ Desafios/Desafio1/javaScript.js
@@ -7,30 +7,31 @@ var boxshadow = document.getElementById('Container')
 var icon = document.getElementById('icon')
 time.innerHTML = `Agora são ${hours}:${minutes}`
 
-if (hours >0 && hours<=5){
+if (hours > 0 && hours <= 5) {
 
   stats.innerHTML = 'Boa Madrugada'
-  document.body.style.backgroundImage = "url('imagens/Dawn.jpg')"
+  document.body.style.backgroundImage = "url('image/Dawn.jpg')"
   boxshadow.style.boxShadow = '0px 0px 20px 5px var(--cor1)'
   icon.innerHTML = "<img src='icon/icons8-sol.svg' alt=''></img>"
 
-} else if (hours >5 && hours <12){
+} else if (hours > 5 && hours < 12) {
 
   stats.innerHTML = 'Bom Dia'
-  document.body.style.backgroundImage = "url('imagens/day.jpg')"
+  document.body.style.backgroundImage = "url('image/day.jpg')"
   boxshadow.style.boxShadow = '0px 0px 20px 5px var(--cor2)'
 
-} else if (hours >=12 && hours <18){
+} else if (hours >= 12 && hours < 18) {
 
   stats.innerHTML = 'Boa Tarde'
-  document.body.style.backgroundImage = "url('imagens/afternoon.jpg')"
+  document.body.style.backgroundImage = "url('image/afternoon.jpg')"
   boxshadow.style.boxShadow = '0px 0px 20px 5px var(--cor3)'
 
-} else {
+} else if (hours >= 18 && hours <= 23) {
 
   stats.innerHTML = 'Boa Noite'
-  document.body.style.backgroundImage = "url('night.jpg')"
+  document.body.style.backgroundImage = "url('image/night.jpg')"
   boxshadow.style.boxShadow = '0px 0px 20px 5px var(--cor4)'
-  icon.innerHTML= "<img src='icon/Night_Landscape_icon-icons.com_54161.svg' alt=''></img>"
+  icon.innerHTML = "<p>&#x1F311<p>"
+
 
 }
